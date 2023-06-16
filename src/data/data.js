05debcbcd260196de103fac0d24d164bb2d63b9a
jaafar mohammad <jafarmh777@gamil.com>
2023-06-16 23:03:56 +0200

    fs

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"E-commerce store",
+        image:WorkIm,
+        github:"https://github.com/Jaafarmh/crwn-clothing-v2-redux-toolkit",
+        live:"https://ecommerce-store-final.netlify.app",
+    },
+    {
+        id:2,
+        name:"LinkedIn Clone",
+        image:RealEst,
+        github:"https://github.com/Jaafarmh/LinkedIn/tree/master",
+        live:"https://my-own-linkedin.netlify.app",
+    },
+    
+
+
+]
