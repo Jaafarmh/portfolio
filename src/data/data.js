26ce8086d7a99c37f8b497d0990f4a6cd0d75793
jaafar mohammad <jafarmh777@gamil.com>
2023-06-27 19:39:49 +0200

    final step

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,7 @@
 import LinkedIn from '../assets/projects/LinkedIn.png'
 import Ecommerce from '../assets/projects/Ecommerce.png'
+import Netflix from '../assets/projects/Netflix.png'
+
 export const data=[
     {
         id:1,
@@ -14,6 +16,13 @@ export const data=[
         image:LinkedIn,
         github:"https://github.com/Jaafarmh/LinkedIn/tree/my-main",
         live:"https://mylinkedin-clone1.netlify.app",
+    },
+    {
+        id:3,
+        name:"Netflix Clone",
+        image:Netflix,
+        github:"https://github.com/Jaafarmh/Netflix-clone/tree/my-main",
+        live:"https://netflix-1clone12.netlify.app",
     },
     
 
